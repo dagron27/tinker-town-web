dc5a259cc5f9a1ea4f43f30c5c5f86ae3e9c73b5
daniel-leone <123667871+dagron27@users.noreply.github.com>
2026-09-17 16:07:52 -0500

    build: verified web export with test harness integration

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789677200|4047181';
+const CACHE_VERSION = '1789678769|4897290';
 /** @type {string} */
 const CACHE_PREFIX = 'Tinker Town-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
